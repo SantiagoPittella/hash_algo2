60cdec45b86c0b1770f6356bdd0b40585568af1b
SantiPittella <pittellasantiago@gmail.com>
2020-06-21 19:04:35 -0300

    estructura inicial

--- hash.c
+++ hash.c
@@ -32,8 +32,15 @@ struct hash_iter {
     size_t num_lista;  //revisar, indicaria la posicion dentro del hash de la lista actual.
 };
 
-unsigned long funcion_hash();
+unsigned long funcion_hash();  //buscar alguna de las que tengo guardadas en internet.
 
+/*
+Inicializa las lista, para cada posicion del hash, 
+cuando haciendo mas facil chequear si ya esta o no una clave, 
+pues si la lista esta vacia, no esta la clave directamente.
+Ademas, no hay que crear una lista cada vez que se quiere guardar algo.
+Esta funcion tambien se puede usar a la hora de redimensionar la tabla.
+*/
 bool _inicializar_listas(lista_hash_t** campos, size_t cantidad) {
     for (size_t i = 0; i < cantidad; i++) {
         campos[i] = lista_hash_crear();
@@ -41,6 +48,7 @@ bool _inicializar_listas(lista_hash_t** campos, size_t cantidad) {
     }
     return true;
 }
+
 hash_t* hash_crear(hash_destruir_dato_t destruir_dato) {
     hash_t* hash = malloc(sizeof(hash));
     if (!hash) return NULL;
@@ -68,4 +76,35 @@ size_t hash_cantidad(const hash_t* hash) {
 
 bool hash_pertenece(const hash_t* hash, const char* clave) {
     return (hash_obtener(hash, clave) != NULL);
+}
+
+/*
+    PARA COMPARAR CLAVES, USAR strcmp()
+    (en hash_obtener y hash_guardar)
+*/
+void* hash_obtener(const hash_t* hash, const char* clave) {
+    /*
+        f_hash(clave), recorrer lista, si no esta clave, devolver NULL, 
+        si esta, devolver dato.
+    */
+}
+
+bool hash_guardar(hash_t* hash, const char* clave, void* dato) {
+    /*
+        f_hash(clave) para obtener posicion de la lista a buscar,
+        si la clave ya esta en la lista, cambiar dato.
+        SI la clave no esta en la lista, lista_insertar(clave, dato) (mejor insertar al final, creo).
+        En caso de error insertando devuelve false.
+
+    */
+    return true;
+}
+
+void hash_destruir(hash_t* hash) {
+    /*
+    Recorrer todas las listas, borrando las claves (las copiamos), 
+    luego borrar las listas.
+    free(hash->campos)
+    free(hash)
+    */
 }
--- lista_hash.c
+++ lista_hash.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 #include "string.h"
+
 typedef struct nodo {
     struct nodo* proximo;
     void* dato;
